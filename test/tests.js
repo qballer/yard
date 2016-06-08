ca1f398102d23e844ce7a34ae9082f2385f6b397
Doron Tsur <qballer@gmail.com>
2016-06-08 23:30:06 +0300

    minor fixes

--- test/tests.js
+++ test/tests.js
@@ -1,4 +1,3 @@
-var useCases  = require('./samples');
 var expect    = require("chai").expect;
 var samples   = require('./samples');
 var resolver  = require('../src');
@@ -6,8 +5,7 @@ var resolver  = require('../src');
 describe('should support the expression', function(){
 	samples.forEach(function(element) {
 		it(element.exp, function() {
-			var actualResult = resolver(element.exp);
-			expect(actualResult.toString()).to.equal(element.result);
+			expect((resolver(element.exp)).toString()).to.equal(element.result);
 		});
 	});
 
