e2e6c5432f0fd920a53cd126578b671ca78a95fe
Doron Tsur <qballer@gmail.com>
2016-06-08 17:46:46 +0300

    remove console.logs

--- src/operators.js
+++ src/operators.js
@@ -2,35 +2,30 @@ module.exports = {
 	'-': {
 		'precedence': 2,
 		'operation': function(number1, number2){
-			// console.log(number2 + ' - ' + number1);
 			return number2 - number1;
 		}
 	},
 	'+': {
 		'precedence': 2,
 		'operation': function(number1, number2){
-			// console.log(number2 + ' + ' + number1);
 			return number2 + number1;
 		}
 	},
 	'*': {
 		'precedence': 3,
 		'operation': function(number1, number2){
-			// console.log(number2 + ' * ' + number1);
 			return number1 * number2;
 		}
 	},
 	'/': {
 		'precedence': 3,
 		'operation': function(number1, number2){
-			// console.log(number2 + ' / ' + number1);
 			return number2 / number1;
 		}
 	},
 	'^': {
 		'precedence': 4,
 		'operation': function(number1, number2){
-			// console.log(number2 + ' ^ ' + number1);
 			return Math.pow(number2, number1) ;
 		}
 	},
