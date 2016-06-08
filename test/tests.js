2c47f7baae6ed5995d00cb1b4205e600319ba48a
Doron Tsur <qballer@gmail.com>
2016-06-08 17:49:42 +0300

    simple change

--- test/tests.js
+++ test/tests.js
@@ -3,7 +3,7 @@ var expect    = require("chai").expect;
 var samples   = require('./samples');
 var resolver  = require('../src');
 
-describe('should support the expression ', function(){
+describe('should support the expression: ', function(){
 	samples.forEach(function(element) {
 		it(element.exp, function() {
 			var actualResult = resolver(element.exp);
